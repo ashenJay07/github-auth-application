27ba241fa9fe9208724243ecb7b1d0059560e61a
Ashen Jayarathne <ashenj.official@gmail.com>
2024-02-19 00:30:44 +0530

    Implement backend routes to work with GitHub APIs

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,49 @@ var app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// Code being passed from the frontend
+app.get("/getAccessToken", async (req, res) => {
+  console.log(req.query.code);
+
+  // prettier-ignore
+  const params = "?client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&code=" + req.query.code;
+
+  await fetch("https://github.com/login/oauth/access_token" + params, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      return response.json;
+    })
+    .then((data) => {
+      console.log(data);
+      res.json(data);
+    });
+});
+
+// Getting user data
+// Access token is going to be passed in as an Authorization header
+app.get("/getUserData", async (req, res) => {
+  req.get("Authorization"); // Bearer type 'Access_Token'
+
+  await fetch("https://api.github.com/user", {
+    method: "GET",
+    headers: "GET",
+    headers: {
+      Authorization: req.get("Authorization"),
+    },
+  })
+    .then((response) => {
+      return response.json;
+    })
+    .then((data) => {
+      console.log(data);
+      res.json(data);
+    });
+});
+
 app.listen(8000, () => {
   console.log(`Server listening on port 8000...`);
 });
